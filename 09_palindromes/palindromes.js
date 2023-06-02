b29250bff079f92318d84ead2368ec3c56796d0e
Aiman <aiman991026@gmail.com>
2023-06-03 03:10:08 +0800

    Done javascript exercise palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reverseStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
