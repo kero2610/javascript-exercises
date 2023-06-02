929a70ac089c0a77301366f3777ac3830947ee3e
Aiman <aiman991026@gmail.com>
2023-06-03 03:34:36 +0800

    Done javascript exercise fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const sequence = [0, 1];
 
+    if (n < 0){
+        return 'OOPS';
+    }
+    else if (n === 1 || n === 0) {
+      return [0];
+    } 
+    else if (n === 2) {
+      return sequence[n];
+    }
+  
+    for (let i = 2; i < n+1; i++) {
+      const nextNumber = sequence[i - 1] + sequence[i - 2];
+      sequence.push(nextNumber);
+    }
+  
+    return sequence[n];
 };
 
 // Do not edit below this line
