783dfcdd26f7de30b73f2e916afd5e1116f48693
Aiman <aiman991026@gmail.com>
2023-06-03 05:01:19 +0800

    Done exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce(function(oldest, currentPerson){
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        const oldestPersonAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        if(currentPersonAge > oldestPersonAge){
+            return currentPerson;
+        }
+        return oldest;
+    });
 };
+
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
